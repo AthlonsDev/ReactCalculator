cb0a7872cfcdb060e2322be2df52cecac3fb798b
AthlonsDev <47662221+AthlonsDev@users.noreply.github.com>
2022-10-27 23:34:17 +0100

    Adding Functionalty with reducer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import './App.css';
+import {useReducer} from 'react';
+import DigitButton from './DigitButton';
+
+
+const ACTIONS = {
+    ADD_DIGIT: 'add-digit',
+    CLEAR: 'clear',
+    DELETE_DIGIT: 'delete-digit',
+    CHOOSE_OPERATION: 'choose-operation',
+    EVALUATE: 'evaluate'
+} 
+
+
+function reducer(state, {type, payload}) {
+    switch(type) {
+        case ACTIONS.ADD_DIGIT:
+            return {
+                ...state,
+                currentOperand: {currentOperand} + {payload},
+            }
+    }
+}
 
 function App() {
+    const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer,
+        {})
+
+    dispatch({type: ACTIONS.ADD_DIGIT, payload: {digit: 1}})
     return (
         <div className='calculator-grid'>
             <div className='output'>
-                <div className='previous-operand'>123456</div>
-                <div className='current-operand'>456789</div>
+                <div className='previous-operand'> {previousOperand} {operation}</div>
+                <div className='current-operand'> {currentOperand}</div>
             </div>
             <button className='span-two'>AC</button>
             <button>DEL</button>
-            <button>/</button>
+            <DigitButton digit='/' dispatch={dispatch}/>
             <button>1</button>
             <button>2</button>
             <button>3</button>
